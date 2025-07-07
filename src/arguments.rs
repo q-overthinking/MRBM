1aa0af1a2af88ddb0fecd0bb582c0c00fd7860dc
q-overthinking <170511062+q-overthinking@users.noreply.github.com>
2025-07-07 15:45:58 +0500

    Changes to be committed: 	new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/arguments.rs 	new file:   src/main.rs

---
+++ src/arguments.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "MBRM",
+    version = "1.0",
+    author = "nakugi",
+    about = "a simple mbr management utility in rust"
+)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long)]
+    pub input: String,
+
+    #[arg(short, long, default_value_t = 16, help = "Set amount of symbols in one chunk")]
+    pub chunk_symbols: usize,
+
+    #[arg(short, long, help = "Print blake3 checksum at the end of output")]
+    pub blake3: bool,
+
+    #[arg(short, long, help = "Write MBR data to given path")]
+    pub output: Option<String>,
+
+    #[arg(short, long, help = "Remove color from MBR hex dump")]
+    pub no_color: bool
+}
+
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::{fs::{write, File}, io::{self, Read, Write}};
+use anyhow::{Context, Result};
+use clap::Parser;
+mod arguments;
+use colored::Colorize;
+
+fn main() -> Result<()> {
+    let args = arguments::Args::parse();
+    let input_file  = File::open(args.input).context("failed to open file");
+    let mut buffer = [0; 512];
+    let bytes_read = input_file?.read(&mut buffer).context("failed to read file")?.to_string();
+    match &buffer[..] {
+        [.., a, b] => if a != &85_u8 && b != &170_u8 {
+            panic!("invalid MBR signature")
+        },
+        _ => panic!("invalid MBR signature") 
+    };
+    if let Some(output) = args.output {
+        write(output, buffer)?;
+        return Ok(())
+    }
+    let mbr_data = &mut buffer
+        .chunks(args.chunk_symbols);
+    for (index, chunk) in mbr_data.enumerate() {
+        let hex_dump_offset = index * args.chunk_symbols;
+        writeln!(io::stdout(), "{}:\t{}", if !args.no_color { format!("0x{:0>5x}",hex_dump_offset).red().bold() } else { format!("0x{:0>5x}",hex_dump_offset).bold() }, chunk
+            .iter()
+            .enumerate()
+            .map(|(byte_index, &x)| { 
+                let character = if x < 32_u8 || !x.is_ascii() { 
+                    ".".to_string() 
+                } else {
+                    (x as char).to_string()
+                };
+                if !args.no_color {
+                    match hex_dump_offset + byte_index {
+                        0x00000..0x001BE => character.on_truecolor(178,0,0),
+                        0x001BE..0x001FE => character.on_blue(),
+                        0x001FE if x == 0x55 => character.on_truecolor(178,0,178),
+                        0x001FF if x == 0xAA => character.on_truecolor(178,0,178),
+                        _ => character.on_black(),
+                    }.to_string()
+                } else { character } 
+            })
+            .collect::<Vec<String>>()
+            .join(""))?;
+    }
+    if args.blake3 {
+        let blake3_checksum = blake3::hash(&buffer).to_string();
+        writeln!(io::stdout(), "blake3: {}", if !args.no_color { blake3_checksum.red().bold() } else { blake3_checksum.bold() })?;
+    }
+    writeln!(io::stdout(), "read {} bytes.", if !args.no_color { bytes_read.bold().red() } else { bytes_read.bold() })?;
+    Ok(())
+} 
